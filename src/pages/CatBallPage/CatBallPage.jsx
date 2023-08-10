f17fb71f6ca5a9dfa34136515de88aa050b6ab92
Kostya-Romanovskyi <romanovskiyk97@gmail.com>
2023-08-10 17:19:57 +0300

    fixed img, added ico and title

--- src/pages/CatBallPage/CatBallPage.jsx
+++ src/pages/CatBallPage/CatBallPage.jsx
@@ -1,35 +1,35 @@
-import CatBallHero from '../../components/CatBallHero/CatBallHero'
-import CatBallGoods from '../../components/CatBallGoods/CatBallGoods'
-import CatBallInfoList from '../../components/CatBallInfoList/CatBallInfoList'
-import CatBallImages from '../../components/CatBallImages/CatBallImages'
-import CatBallArea from '../../components/CatBallArea/CatBallArea'
-import CatBallPros from '../../components/CatBallPros/CatBallPros'
-import CatBallVariables from '../../components/CatBallVariables/CatBallVariables'
-import CatBallTech from '../../components/CatBallTech/CatBallTech'
-import CatBallReal from '../../components/CatBallReal/CatBallReal'
-import CatBallToy from '../../components/CatBallToy/CatBallToy'
-import CatBallForm from '../../components/CatBallForm/CatBallForm'
-import Footer from '../../components/Footer/Footer'
-import ScrollToTop from "react-scroll-to-top";
-import '../../App.css'
+import CatBallHero from '../../components/CatBallHero/CatBallHero';
+import CatBallGoods from '../../components/CatBallGoods/CatBallGoods';
+import CatBallInfoList from '../../components/CatBallInfoList/CatBallInfoList';
+import CatBallImages from '../../components/CatBallImages/CatBallImages';
+import CatBallArea from '../../components/CatBallArea/CatBallArea';
+import CatBallPros from '../../components/CatBallPros/CatBallPros';
+import CatBallVariables from '../../components/CatBallVariables/CatBallVariables';
+import CatBallTech from '../../components/CatBallTech/CatBallTech';
+import CatBallReal from '../../components/CatBallReal/CatBallReal';
+import CatBallToy from '../../components/CatBallToy/CatBallToy';
+import CatBallForm from '../../components/CatBallForm/CatBallForm';
+import Footer from '../../components/Footer/Footer';
+import ScrollToTop from 'react-scroll-to-top';
+import '../../App.css';
 
 const CatBallPage = () => {
-    return (
-        <>
-            <CatBallHero />
-            <CatBallGoods />
-            <CatBallInfoList />
-            <CatBallImages />
-            <CatBallArea />
-            <CatBallPros />
-            {/* <CatBallVariables /> */}
-            {/* <CatBallTech /> */}
-            {/* <CatBallReal /> */}
-            {/* <CatBallToy /> */}
-            <CatBallForm />
-            <Footer />
-            <ScrollToTop smooth className='toTop' />
-        </>
-    )
-}
-export default CatBallPage
+  return (
+    <>
+      <CatBallHero />
+      <CatBallGoods />
+      <CatBallInfoList />
+      <CatBallImages />
+      <CatBallArea />
+      <CatBallPros />
+      {/* <CatBallVariables /> */}
+      {/* <CatBallTech /> */}
+      {/* <CatBallReal /> */}
+      {/* <CatBallToy /> */}
+      <CatBallForm />
+      <Footer />
+      <ScrollToTop smooth className="toTop" />
+    </>
+  );
+};
+export default CatBallPage;
